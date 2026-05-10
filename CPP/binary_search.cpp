53f536f820939b473cf05d9b9411b24fe6e74adc
aashish890bit <aashish890.bit@gmail.com>
2026-05-10 14:56:53 +0530

    Update binary_search.cpp

--- CPP/binary_search.cpp
+++ CPP/binary_search.cpp
@@ -1,16 +1,17 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 int binarySearch(vector<int>& arr, int target) {
-    int low = 1;
+    int low = 0;
     int high = arr.size() - 1;
-    while(low < high) {
-        int mid = low + high / 2;
+    while(low <= high) {
+        int mid = (low + high) / 2;
         if(arr[mid] == target)
             return mid;
         else if(arr[mid] < target)
             low = mid + 1;
         else
-            high = mid + 1;
+            high = mid - 1;
     }
     return -1;
 }
